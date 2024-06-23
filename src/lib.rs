4d385030e9ec2d0081b3cc7b563250eb8ae0a77d
Refa Andhika <refandika@gmail.com>
2024-06-23 17:43:13 +0700

    Add storage function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+    program_error::ProgramError,
+};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct InstructionData {
+    pub data: u64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
+ 
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instruction_data = InstructionData::try_from_slice(instruction_data)
+        .map_err(|_| ProgramError::InvalidInstructionData)?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let accounts_iter = &mut accounts.iter();
+    let storage_account = next_account_info(accounts_iter)?;
+
+    if storage_account.owner != program_id {
+        msg!("Storage account is not owned by the program");
+        return Err(ProgramError::IncorrectProgramId);
     }
+
+    let mut data = storage_account.try_borrow_mut_data()?;
+    data[..8].copy_from_slice(&instruction_data.data.to_le_bytes());
+
+    msg!("Data stored successfully: {}", instruction_data.data);
+    Ok(())
 }
